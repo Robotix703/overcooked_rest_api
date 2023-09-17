4ee7db9385e58e32407f5362713f1062783c88cd
Robotix703 <stephane.chatelle@orange.fr>
2023-09-17 17:57:19 +0000

    Try using local mongodb

--- src/BDD.ts
+++ src/BDD.ts
@@ -3,9 +3,9 @@ require('dotenv').config()
 
 let uri: string;
 if (process.env.NODE_ENV === "production") {
-    uri = "mongodb://localhost:27017/overcookedProd"
+    uri = "mongodb://mongo_overcooked:27017/overcookedProd"
 }else{
-    uri = "mongodb://localhost:27017/overcookedDev"
+    uri = "mongodb://mongo_overcooked:27017/overcookedDev"
 }
 
 export async function connectToDataBase(){
