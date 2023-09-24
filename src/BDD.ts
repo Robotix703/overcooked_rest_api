646bd9443fe5277209a4c3b74fbc5fd8d42cec32
Robotix703 <stephane.chatelle@orange.fr>
2023-09-24 11:27:32 +0000

    Update dev connection to bdd

--- src/BDD.ts
+++ src/BDD.ts
@@ -5,7 +5,7 @@ let uri: string;
 if (process.env.NODE_ENV === "production") {
     uri = "mongodb://" + process.env.DB_PROD_USER + ":" + process.env.DB_PROD_PASS + "@" + process.env.DB_HOST + ":27017/" + process.env.DB_PROD_NAME + "?authSource=admin";
 }else{
-    uri = "mongodb://" + process.env.DB_DEV_USER + ":" + process.env.DB_DEV_PASS + "@" + process.env.DB_HOST + ":27017/" + process.env.DB_DEV_NAME + "?authSource=admin";
+    uri = "mongodb://localhost:27017/" + process.env.DB_DEV_NAME + "?authSource=admin";
 }
 
 export async function connectToDataBase(){
